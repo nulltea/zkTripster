8ca7a1b54941b3866b77b0f2534c0ff24b472924
Anton Lykhoyda <lykhoyda@gmail.com>
2024-05-25 21:50:03 +0200

    update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/zkTripster/',
   plugins: [react()],
   server: {
     open: true,
   },
+  resolve: {
+    preserveSymlinks: true
+  }
 })
