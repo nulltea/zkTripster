b86790ccd6eae99a7d99e49533cd12499a97550c
Adam Gordon <3723289+gordon-to@users.noreply.github.com>
2024-05-25 20:29:44 +0000

    config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/zkTripster/',
+  base: '/',
   plugins: [react()],
   server: {
     open: true,
